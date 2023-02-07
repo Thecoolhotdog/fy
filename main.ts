22bbeb05eaca5616fc6cbef6c390247f0086de33
Thecoolhotdog <124712995+Thecoolhotdog@users.noreply.github.com>
2023-02-07 18:24:34 +0100

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,61 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # # . .
+        . # # # .
+        . . # # #
+        . # . # #
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        # # # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . # #
+        . . # # #
+        . # # # .
+        . # # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        # # . # .
+        # # # . .
+        . # # # .
+        . . # # .
+        `)
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # # # #
+        # # . . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . # # # .
+        # # # . .
+        # # . # .
+        . . . . #
+        `)
+})
+basic.forever(function () {
+	
+})
